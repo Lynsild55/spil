344bbd84420521947e0b5b95e0f163bca64b995e
silasskovchristensen <silasskovchristensen@gmail.com>
2023-10-02 09:15:30 +0200

    lidt ændringer

--- src/ServerInput.java
+++ src/ServerInput.java
@@ -1,12 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class ServerInput extends Thread {
 
     private Socket socket;
+    private String clientSentence;
 
     public ServerInput(Socket socket) {
         this.socket = socket;
     }
     @Override
-    public void run() {}
+    public void run() {
+        while (true) {
+            try {
+                BufferedReader inFromClient = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                clientSentence = inFromClient.readLine();
+                System.out.println(clientSentence);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 }
