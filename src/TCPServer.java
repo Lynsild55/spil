a79f931ccbd727a71acbe2e329952b7bcf2346d0
holml <holmloch@gmail.com>
2023-10-02 09:14:42 +0200

    yo

--- src/TCPServer.java
+++ src/TCPServer.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -6,8 +9,16 @@ public class TCPServer {
         ServerSocket welcomSocket = new ServerSocket(6789);
         Socket connectionSocket = welcomSocket.accept();
 
-        Input i = new Input(connectionSocket);
-        i.start();
+        while (true) {
+            BufferedReader input = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+            String tekst = input.readLine();
+            System.out.println(tekst);
+            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+            outToClient.writeBytes("Ekko: " + tekst);
+        }
+
+        //Input i = new Input(connectionSocket);
+        //i.start();
         //Output o = new Output(connectionSocket);
         //o.start();
 
